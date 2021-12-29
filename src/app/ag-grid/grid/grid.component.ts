94cde3ca7029668f9fc453a1dfde5ae1a15be196
Riley <rwaugh52@gmail.com>
2021-12-28 23:37:26 -0800

    Adding AgGridwith data

---
+++ src/app/ag-grid/grid/grid.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
+import { CustomFilterComponent } from '../custom-filter/custom-filter.component';
+
+@Component({
+  selector: 'app-grid',
+  templateUrl: './grid.component.html',
+  styleUrls: ['./grid.component.scss']
+})
+export class GridComponent {
+
+  frameworkComponents = {
+    customFilter: CustomFilterComponent,
+  };
+
+  defaultColDef: ColDef = {
+    sortable: true,
+    filter: true,
+  }
+
+  columnDefs: ColDef[] = [
+    { field: 'make', filterFramework: CustomFilterComponent },
+    { field: 'model' },
+    { field: 'ponies'}
+  ];
+
+  rowData = [
+    { make: 'Volkswagen', model: 'Golf', ponies: 147 },
+    { make: 'Volkswagen', model: 'Golf GTI', ponies: 241 },
+    { make: 'Volkswagen', model: 'Golf R', ponies: 315 },
+    { make: 'Audi', model: 'RS3', ponies: 394 },
+    { make: 'Subaru', model: 'Impreza', ponies: 152 },
+    { make: 'Subaru', model: 'Impreza WRX', ponies: 268 },
+    { make: 'Subaru', model: 'Impreza WRX STI', ponies: 305 },
+    { make: 'Subaru', model: 'BRZ', ponies: 228 },
+    { make: 'Ford', model: 'Focus', ponies: 160 },
+    { make: 'Ford', model: 'Focus ST', ponies: 252 },
+    { make: 'Ford', model: 'Focus RS', ponies: 350 },
+    { make: 'Tesla', model: 'Model 3', ponies: 252 },
+    { make: 'Tesla', model: 'Model 3 Performance', ponies: 430 },
+    { make: 'Smart', model: 'fortwo', ponies: 89 },
+  ];
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GridComponent } from './ag-grid/grid/grid.component';
+import { AgGridModule } from 'ag-grid-angular';
+import { CustomFilterComponent } from './ag-grid/custom-filter/custom-filter.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridComponent,
+    CustomFilterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
